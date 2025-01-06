eb23c0f5e5e49500fb21239f813117a0b9d00194
Mohammad Idrees <mohammadidrees.mohammadi786@gmail.com>
2025-01-06 09:40:08 +0430

    React Router Dom Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import WelcomePage from '../Pages/Welcome/WelcomePage'
+import Home from '../Pages/Home/Home'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <WelcomePage />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  }
+])
+
+
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
